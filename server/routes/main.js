b286d80020662bacf435c315e82bc6d8da1c924b
jsmld <samvanerpersroijaards@gmail.com>
2023-09-15 10:57:45 -0300

    Adds pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -7,19 +7,54 @@ const Post = require('../models/Post');
  * HOME
 */
 router.get('/', async (req, res) => {
-  const locals = {
-    title: "SVER Blog",
-    description: "A blog where I share my learnings"
-  };
-
   try {
-    const data = await Post.find();
-    res.render('index', { locals, data });
+    const locals = {
+      title: "SVER Blog",
+      description: "A blog where I share my learnings"
+    };
+
+    let perPage = 5;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+                           .skip(perPage * page - perPage)
+                           .limit(perPage)
+                           .exec();
+
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('index', {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null
+    });
   } catch (error) {
     console.log(error);
   }
 
 });
+
+
+
+// WITHOUT PAGINATION
+
+// router.get('/', async (req, res) => {
+//   const locals = {
+//     title: "SVER Blog",
+//     description: "A blog where I share my learnings"
+//   };
+
+//   try {
+//     const data = await Post.find();
+//     res.render('index', { locals, data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// });
 
 
 
