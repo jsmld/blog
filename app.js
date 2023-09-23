f0ed96bb6582d6cfb0c43ef973177b1db9648c88
jsmld <samvanerpersroijaards@gmail.com>
2023-09-23 18:19:43 -0300

    Adds authentication

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
+const { application } = require('express');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
@@ -13,6 +17,18 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+  secret: 'bonkers',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  cookie: { maxAge: new Date(Date.now() + (3600000)) }
+
+}))
 
 app.use(express.static('public'));
 
@@ -23,6 +39,7 @@ app.set('view engine', 'ejs');
 
 
 app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 
 app.listen(PORT, () => {
